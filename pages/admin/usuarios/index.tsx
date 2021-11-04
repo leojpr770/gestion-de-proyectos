94a2d59b36b6376daa26bf94adc568a262d56cdb
leo <leojpr770@gmail.com>
2021-11-04 11:38:57 -0500

    agregar telwind y rutas de muestrs

---
+++ pages/admin/usuarios/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const leonel = () => {
+    return <div>pagina admin de usuarios
+    </div>;
+};
+
+export default leonel
---
+++ pages/test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const test = () => {
+    return <div>hola, esta es la pagin  de leonel
+    </div>;
+};
+
+export default test
